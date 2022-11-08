111c9a5ab4822b02c6eeb4ecaa7b13f47cc07337
Diego Lopez <diegologu@unisabana.edu.co>
2022-11-08 18:05:09 -0500

    Create arduino_mock.ino

---
+++ arduino_mock/arduino_mock/arduino_mock.ino
@@ -0,0 +1,23 @@
+void setup() {
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  Serial.println(F("289,1,4,29,12,0,650,486,19,1"));
+  delay(1000);
+  Serial.println(F("304,1,4,29,12,0,731,486,19,1"));
+  delay(1000);
+  Serial.println(F("317,1,4,29,12,0,812,486,19,1"));
+  delay(1000);
+  Serial.println(F("327,1,4,29,12,1,0,462,19,1"));
+  delay(1000);
+  Serial.println(F("327,1,4,29,12,1,0,0,19,1"));
+  delay(1000);
+  Serial.println(F("324,1,4,29,12,1,0,0,19,1"));
+  delay(1000);
+  Serial.println(F("321,1,4,29,12,1,0,0,19,1"));
+  delay(1000);
+
+}
+
