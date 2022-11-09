414e68baf9b29f720bd87cb0607ba4da19ff6f5e
Otalvaro1 <116774087+Otalvaro1@users.noreply.github.com>
2022-11-09 17:58:47 -0500

    Update arduino_mock.ino

--- arduino_mock/arduino_mock.ino
+++ arduino_mock/arduino_mock.ino
@@ -5,19 +5,18 @@ void setup() {
 
 void loop() {
   Serial.println(F("289,1,4,29,12,0,650,486,19,1"));
-  delay(1000);
+  delay(200);
   Serial.println(F("304,1,4,29,12,0,731,486,19,1"));
-  delay(1000);
+  delay(200);
   Serial.println(F("317,1,4,29,12,0,812,486,19,1"));
-  delay(1000);
+  delay(200);
   Serial.println(F("327,1,4,29,12,1,0,462,19,1"));
-  delay(1000);
+  delay(200);
   Serial.println(F("327,1,4,29,12,1,0,0,19,1"));
-  delay(1000);
+  delay(200);
   Serial.println(F("324,1,4,29,12,1,0,0,19,1"));
-  delay(1000);
+  delay(200);
   Serial.println(F("321,1,4,29,12,1,0,0,19,1"));
-  delay(1000);
+  delay(200);
 
 }
-
